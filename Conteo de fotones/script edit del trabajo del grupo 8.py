1417651fe79f5657601c1eb1af4e1c79d24ddb8e
MaxDaniels77 <45187572+MaxDaniels77@users.noreply.github.com>
2021-10-22 01:43:12 -0300

    version 3
    Algunos updates en el script

--- Conteo de fotones/script edit del trabajo del grupo 8.py
+++ Conteo de fotones/script edit del trabajo del grupo 8.py
@@ -14,70 +14,6 @@ from scipy.stats import poisson
 import pandas as pd
 from scipy.optimize import curve_fit
 os.chdir('D:\Documentos\Documentos-facultad\Labo 5\Laboratorio-5\Conteo de fotones')
-# Pantalla = np.loadtxt('Foton BE.csv', delimiter = ',')
-# Pantalla = np.loadtxt('Foton BE.csv', delimiter = ',')
-Pantalla = np.load(r'D:/Documentos/Documentos-facultad/Labo 5/Laboratorio-5/Conteo de fotones/Grupo v3 dia 2 disco d/Grupo v3/1000 ventanas encendido 10mv 1us.npz')
-prelim = np.loadtxt(r'D:/Documentos/Documentos-facultad/Labo 5/Laboratorio-5/Conteo de fotones/Grupo 3v/data_preliminar_100ohm 25ns.txt',delimiter=',')
- 
-for item in Pantalla.keys():
-    print(item)
-
-for item in Pantalla.values():
-    print(item)
-    
-#%%
-plt.figure(1,figsize=(10,6))
-plt.clf()
-plt.rc('font', family='serif', size=13)
-fig, ax = plt.subplots(1,1,num=1, sharex=True)
-# 1 Muestro la referencia.
-ax.plot(prelim[0], prelim[1], label='Osc. screen')
-ax.set_xlabel("Tiempo[s]")
-ax.set_ylabel("Voltaje[V]")
-ax.ticklabel_format(axis='y',style='sci',scilimits=(1,4))
-ax.set_title('Pulso 25 ns')
-ax.legend(loc='lower right')
-ax.grid(True)
-ax.set_xlim(prelim[0].min(),prelim[0].max())
-# Ordenamos y salvamos la figura.
-plt.tight_layout()
-plt.show()  
-plt.savefig('pulso 25 ns.png')
-
-#%% DATOS CRUDOS
-
-Pantalla = Pantalla['screens']
-plt.plot(range(len(Pantalla)), Pantalla)
-plt.title("Pulso")
-plt.ylabel("Voltaje[V]")
-plt.xlabel("Tiempo[s]")
-# plt.xlim([0,0.010])
-plt.grid(True)
-
-#%% HISTOGRAMA DE LOS DATOS CRUDOS
-plt.figure(1,figsize=(8,6))
-plt.clf()
-plt.rc('font', family='serif', size=13)
-fig, ax = plt.subplots(1,1,num=1, sharex=True)
-# 1 Muestro la referencia.
-ax.hist(Pantalla ,bins = 70, label='Histograma')
-ax.set_xlabel("Alturas [mV]")
-ax.set_ylabel("Apariciones")
-ax.ticklabel_format(axis='y',style='sci',scilimits=(1,4))
-ax.set_title('Histograma de datos crudos')
-ax.set_yscale('log')
-ax.legend()
-ax.grid(True)
-# Ordenamos y salvamos la figura.
-plt.tight_layout()
-plt.show()  
-plt.savefig('Histograma de cuentas apagadas.png')
-#%%
-
-escala = 1.25 # microsegundos
-resolucion = escala/250*1e-6 # Consutla
-tiempo = np.arange(0, resolucion*(2500*500), resolucion)
-data=Pantalla
 
 #%% Funciones
 
@@ -136,6 +72,104 @@ def busca_picos(Intensidades, delta, umbral):
     return np.array(ubi_picos)
 
 #%%
+
+# Pantalla = np.loadtxt('Foton BE.csv', delimiter = ',')
+# Pantalla = np.loadtxt('Foton BE.csv', delimiter = ',')
+Pantalla = np.load(r'D:/Documentos/Documentos-facultad/Labo 5/Laboratorio-5/Conteo de fotones/Grupo v3 dia 2 disco d/Grupo v3/1000 ventanas apagado 10mv 1us.npz')
+prelim = np.loadtxt(r'D:/Documentos/Documentos-facultad/Labo 5/Laboratorio-5/Conteo de fotones/Grupo 3v/data_preliminar_100ohm 25ns.txt',delimiter=',')
+ 
+for item in Pantalla.keys():
+    print(item)
+
+for item in Pantalla.values():
+    print(item)
+    
+#%%
+
+plt.figure(1,figsize=(10,6))
+plt.clf()
+plt.rc('font', family='serif', size=13)
+fig, ax = plt.subplots(1,1,num=1, sharex=True)
+# 1 Muestro la referencia.
+ax.plot(prelim[0], prelim[1], label='Osc. screen')
+ax.set_xlabel("Tiempo[s]")
+ax.set_ylabel("Voltaje[V]")
+ax.ticklabel_format(axis='y',style='sci',scilimits=(1,4))
+ax.set_title('Pulso 25 ns')
+ax.legend(loc='lower right')
+ax.grid(True)
+ax.set_xlim(prelim[0].min(),prelim[0].max())
+# Ordenamos y salvamos la figura.
+plt.tight_layout()
+plt.show()  
+plt.savefig('pulso 25 ns.png')
+
+#%% DATOS CRUDOS
+
+Pantalla = Pantalla['screens']
+plt.plot(range(len(Pantalla)), Pantalla)
+plt.title("Pulso")
+plt.ylabel("Voltaje[V]")
+plt.xlabel("Tiempo[s]")
+# plt.xlim([0,0.010])
+plt.grid(True)
+
+#%% HISTOGRAMA DE LOS DATOS CRUDOS DE LAS CUENTAS APAGADAS
+
+plt.figure(1,figsize=(8,6))
+plt.clf()
+plt.rc('font', family='serif', size=13)
+fig, ax = plt.subplots(1,1,num=1, sharex=True)
+# 1 Muestro la referencia.
+a = ax.hist(Pantalla*1e3 ,bins = 70, label='Histograma')
+ax.set_xlabel("Alturas [mV]")
+ax.set_ylabel("Apariciones")
+ax.ticklabel_format(axis='y',style='sci',scilimits=(1,4))
+ax.set_title('Histograma de datos crudos')
+# ax.set_yscale('log')
+ax.legend()
+ax.grid(True)
+# Ordenamos y salvamos la figura.
+plt.tight_layout()
+plt.show()  
+plt.savefig('Histograma de cuentas apagadas.png')
+
+#%% AJUSTE DE GAUSSIANA DE CUENTAS OSCURAS
+
+'''AL usar plt.hist como se configuro previamente es necesario matar los puntos que quedan
+pegados en y = 0 y tomar los maximos correspondientes, para esto se realiza el siguiente 
+crop en los datos'''
+
+x = a[1][np.where(a[0]>0)]
+y = a[0][np.where(a[0]>0)]
+print(y.max())
+# y = y/y.max() # normalice para que el algoritmo converja mejor
+
+# plt.plot(x,y,".") # Chequeo que lo que obtuve sea correcto
+
+parametros_optimos, matriz_de_cov = curve_fit(gauss, x, y, p0=[500000,0.6, 0.8])
+error = np.sqrt(np.diag(matriz_de_cov))
+X = np.linspace(x.min(),x.max(),1000)
+plt.plot(x,y,".") 
+plt.plot(X,gauss(X,*parametros_optimos))
+print(f' Parametros = {parametros_optimos}\n errores={error}')
+
+#%%
+
+Pantalla = np.load(r'D:/Documentos/Documentos-facultad/Labo 5/Laboratorio-5/Conteo de fotones/Grupo v3 dia 2 disco d/Grupo v3/1000 ventanas encendido 10mv 1us.npz')
+
+for item in Pantalla.keys():
+    print(item)
+
+for item in Pantalla.values():
+    print(item)
+    
+data=Pantalla['screens']
+escala = 1.25 # microsegundos
+resolucion = escala/250*1e-6 # Consutla
+tiempo = np.arange(0, resolucion*(len(data)), resolucion)
+
+#%%
 alturas = []
 t_a = []
 delta = 2
@@ -153,7 +187,9 @@ for index in ubi_picos2:
     t_a.append(tiempo[index])    
 
 print(len(alturas))
+
 #%%
+
 plt.figure()
 plt.plot(tiempo, data)
 # plt.plot(tiempo[0:len(tiempo)]*1e6, data[0:len(tiempo)]*1e3)
@@ -164,15 +200,20 @@ plt.xlabel("Tiempo[us]")
 # plt.xlim([0, 4])
 # plt.ylim([-10, 2])
 plt.grid(True)
+#%% ALTURAS RECORTANDO LOS PICOS
+t = np.array(t_a[0:len(t_a)])
+peak = np.array(alturas[0:len(t_a)])*1e2
+# peak_cut_index = np.where(peak<-) 
+plt.plot(t,peak)
 #%% HISTOGRAMA DE LOS DATOS CRUDOS (NO PICOS)
 
 plt.figure()
-plt.hist(Pantalla*100,bins=200)
+plt.hist(data*100,bins=200)
 plt.grid(True)
 plt.legend()
 plt.xlabel('Alturas[mV]')
 plt.ylabel('Apariciones')    
-plt.title('Histograma de picos')
+plt.title('Histograma de los datos')
 plt.yscale("log")
 # plt.ylim([100, 100000])
 
@@ -221,18 +262,25 @@ ax[1].vlines(-2.5,100,1000000,ls="--",label='Threshold') , ax[1].legend()
 plt.tight_layout()
 plt.show()  
 plt.savefig('histograma edit propio.png')
+#%%
+
+
+
+
 #%% RECORTANDO EN EL UMBRAL FIJADO A OJO EN -2.5
 #    recortando en el umbralllll
+
 data_escalada = data*1e3
 index_recorte = np.where(data_escalada<-2.5)
 # plt.hist(data_escalada[index_recorte],bins=100)
 data = data[index_recorte]
 tiempo = tiempo[index_recorte]
+
 #%%
+
 alturas = []
 t_a = []
 delta = 2
-
 
 ubi_picos1 = busca_picos(-data, delta, 0.00001 )
 ubi_picos2 = busca_picos(data, delta, 0.00001 )
@@ -246,7 +294,9 @@ for index in ubi_picos2:
     t_a.append(tiempo[index])    
 
 print(len(alturas))
+
 #%%
+
 plt.figure()
 plt.plot(tiempo, data)
 # plt.plot(tiempo[0:len(tiempo)]*1e6, data[0:len(tiempo)]*1e3)
@@ -258,8 +308,8 @@ plt.xlabel("Tiempo[us]")
 # plt.ylim([-10, 2])
 plt.grid(True)
 
-
 #%%
+
 mediciones = np.array(alturas)*1e3
 # mediciones = data*1e3
 DV = 0.08
@@ -345,12 +395,14 @@ plt.ylabel('Voltaje [V]')
 plt.title("Pantalla")
 
 #%%
+
 prom = np.mean(d)
 # prom = 0
 Corre = np.correlate(d - prom, d - prom, mode = "full")
 print(len(Corre))
 
 #%%
+
 paso = t[1]-t[0]
 ti_c = np.linspace(0, paso*2*len(t)-2, 2*len(t)-1)
 plt.plot(ti_c, Corre)
