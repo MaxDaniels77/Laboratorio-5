910c569ef681edd1f6e8b504c03577dce3435062
MaxDaniels77 <45187572+MaxDaniels77@users.noreply.github.com>
2021-11-12 07:52:05 -0300

    se agrego el ajuste del primer modo tem

--- Laser/analisis_laser.py
+++ Laser/analisis_laser.py
@@ -17,7 +17,8 @@ from scipy.special import factorial
 from scipy.stats import poisson
 import pandas as pd
 from scipy.optimize import curve_fit
-
+import imageio
+import cv2
 # os.chdir('D:\Documentos\Documentos-facultad\Labo 5\Laboratorio-5\Laser')
 # %% PLOTEO DE CURVAS
 # os.chdir('D:\Documentos\Documentos-facultad\Labo 5\Laboratorio-5\Laser\Mediciones') # windows
@@ -26,8 +27,7 @@ os.chdir(
 )  # linux
 
 prelim = np.loadtxt(
-    r"Eficiencia de la cavidad lineal P vs I.csv", delimiter=";", skiprows=1
-)
+    r"Eficiencia de la cavidad lineal P vs I.csv", delimiter=",", skiprows=1)
 
 I = prelim[:, 0]
 P = prelim[:, 1]
@@ -70,6 +70,89 @@ plt.title("Ajuste Bose-Einstein")
 grados_lib = len(y) - len(parametros_optimos) - 1
 modelo = lineal(x, *parametros_optimos)
 chi_cuadrado = np.sum(((y - modelo) / error_y) ** 2)  # Cálculo del χ²
+# Cálculo de su p-valor asociado
+p_chi = 1 - chi2.cdf(chi_cuadrado, grados_lib)
+# Printeamos los resultados
+print(f"chi^2: {chi_cuadrado}")
+print(f"Grados de Libertad: {grados_lib}")
+print(f"p-valor del chi^2: {p_chi:.3g}")
+if p_chi < 0.05:
+    print("Se rechaza la hipótesis de que el modelo ajuste a los datos.\n")
+else:
+    print("No se rechaza la hipótesis de que el modelo ajuste a los datos.\n")
+#%% Funciones para modo tem
+'''La idea es hacer que el laser luego de pegar sobre una lente, para amplificar,
+pegue sobre una hoja milimetrada o algo por el estilo para poder caracterizar el modo
+Para eso es la funcion calibrate scale'''
+
+def gauss(x, a, b, c): # para el ajuste
+    return a*np.exp(-(x-b)**2/(2*c**2))
+
+def diff(v):
+    v_diff = []
+    for i in range(1, len(v) - 1):
+        v_diff.append(v[i][0] - v[i - 1][0])
+        
+    return v_diff
+
+#funcion para calibrar la escala del calibre. Toma un vector y devuelve el promedio de las diferencias entre elementos de ese vector
+#junto con su desviación estandar
+def calibrateScale(x):
+    v_diff = []      
+    v_diff = diff(x)
+    mm = np.mean(v_diff)
+    mm_err = 2*np.std(v_diff)
+    return mm, mm_err
+
+
+#kernel en el codigo de Richi (de donde sale este kernel? Porque usando este kernel la convolucion es tan ruidosa?)
+s = [[1, 2, 1],  
+     [0, 0, 0], 
+     [-1, -2, -1]]
+#%% CARGA DE LA IMAGEN A ANALIZAR
+
+path = 'Modo Tem00.jpeg'
+#cargamos la imagen a analizar
+# AA = imageio.imread(path)
+A = cv2.imread(path,0)
+#layer de la imagen como matriz con valores de 0-255
+# A = AA[:,:,1]# tomo un canal que no sature
+
+#grafica la imagen original
+
+
+image = cv2.line(A,(0,915),(1200,915),(255,0,0),2)
+image = cv2. resize(image, (600, 800)) # Resize image.
+window_name='linea'
+cv2.imshow(window_name,image)
+
+#%% TOMA DEL PERFIL Y AJUSTE 
+
+perf = image[459,:] # El perfil se tomo a ojo (al menos para el modo gaussiano)
+
+plt.plot(range(len(perf)),perf)
+
+
+#%%
+y = perf
+
+x = range(len(perf))
+
+parametros_optimos, matriz_de_cov = curve_fit(
+    gauss, x, y, sigma=np.sqrt(y)+1, p0=[500, 629,100])
+error = np.sqrt(np.diag(matriz_de_cov))
+
+plt.errorbar(x, y, yerr=np.sqrt(y), fmt='+', label='Datos')
+plt.plot(x, gauss(x, *parametros_optimos), ".", label='Ajuste')
+# plt.yscale('log')
+print(f' Parametros = {parametros_optimos}\n errores={error}')
+plt.title('Ajuste Gaussiano')
+# Analisis del estadistico χ²
+# esto es: grados de libertad = #mediciones - #Parámetros - 1
+grados_lib = len(y) - len(parametros_optimos)-1
+modelo = gauss(x, *parametros_optimos)
+error_y = np.sqrt(y)
+chi_cuadrado = np.sum(((y - modelo) / error_y)**2)  # Cálculo del χ²
 # Cálculo de su p-valor asociado
 p_chi = 1 - chi2.cdf(chi_cuadrado, grados_lib)
 # Printeamos los resultados
